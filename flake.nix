5274983952aaaba951347846f9a4d0faac401e03
xhos <60789741+xhos@users.noreply.github.com>
2026-03-03 20:50:39 -0500

    refactor: don't user flake-parts

--- flake.nix
+++ flake.nix
@@ -1,60 +1,58 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     git-hooks,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      formatter = pkgs.alejandra;
-
-      checks = {
-        pre-commit = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks.alejandra.enable = true;
-        };
-      };
-
-      devShells = {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            buf
-            nodejs
-            typescript
-            bun
-
-            (writeShellScriptBin "regen" ''
-              rm -rf src/gen/
-              bun buf generate
-            '')
-
-            (writeShellScriptBin "run" ''
-              bun run dev
-            '')
-
-            (writeShellScriptBin "bump-protos" ''
-              git -C proto fetch origin
-              git -C proto checkout main
-              git -C proto pull --ff-only
-              git add proto
-              git commit -m "chore: bump proto files"
-              git push
-            '')
-          ];
-
-          shellHook = self.checks.${system}.pre-commit.shellHook;
+  }: let
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs
+      ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"]
+      (system: f nixpkgs.legacyPackages.${system});
+  in {
+    checks = forAllSystems (pkgs: {
+      pre-commit = git-hooks.lib.${pkgs.system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
         };
       };
     });
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          buf
+          nodejs
+          typescript
+          bun
+
+          (writeShellScriptBin "regen" ''
+            rm -rf src/gen/
+            bun buf generate
+          '')
+
+          (writeShellScriptBin "run" ''
+            bun run dev
+          '')
+
+          (writeShellScriptBin "bump-protos" ''
+            git -C proto fetch origin
+            git -C proto checkout main
+            git -C proto pull --ff-only
+            git add proto
+            git commit -m "chore: bump proto files"
+            git push
+          '')
+        ];
+
+        shellHook = "${self.checks.${pkgs.system}.pre-commit.shellHook}";
+      };
+    });
+  };
 }
